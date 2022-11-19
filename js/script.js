0cb26ecfccf1e542245163866f202bd4deb9e12f
Alfredo Garcia <Asarec@users.noreply.github.com>
2022-11-19 18:50:42 -0500

    adds the class name and score to the totalClassesScores object

--- js/script.js
+++ js/script.js
@@ -144,6 +144,7 @@ function callModal(result) {
                             }
                         });
                     });
+                    totalClassScores[startingClass] = classScore;
                 });
             });
         }
