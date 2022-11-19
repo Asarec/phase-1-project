a6afea94c5b5578c3e476507243fc63055cb57a7
Alfredo Garcia <Asarec@users.noreply.github.com>
2022-11-19 16:52:00 -0500

    added createElement function to effectively create dom elements with various attributes

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form');
+const listOfResults = createElement('ul', {'class': ['search-results-list']});
+const errorMsg = createElement('h3', {
+    'class': ['error'],
+    'content': 'PLEASE TRY AGAIN'
+});
+
+/**
+ * Creates an element with attributes and values.
+ * @param {string} tag - HTML tag to create.
+ * @param {Object} attributes - Attributes to be assigned to the element.
+ * @returns {Object} - Newly created element with specified attributes.
+ */
+function createElement(tag, attributes) {
+    const element = document.createElement(tag);
+
+    Object.keys(attributes).forEach(key => {
+        if (key === 'class') {
+            element.classList.add.apply(element.classList, attributes[key]);
+        } else if (key ==='content') {
+            element.innerText = attributes[key];
+        } else {
+            element.setAttribute(key, attributes[key]);
+        }
+    });
+
+    return element;
+}
